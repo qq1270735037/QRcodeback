065cbb9f9b6de91254a0891974eb043258abf3ea
qunidebanana <74816245+qq1270735037@users.noreply.github.com>
2023-04-05 17:41:47 +0800

    Add files via upload

---
+++ src/main/java/com/code/qrcodeback/test.java
@@ -0,0 +1,18 @@
+package com.code.qrcodeback;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Controller
+/**
+ * 跨域
+ */
+@CrossOrigin
+public class test {
+    @GetMapping("/")
+    @ResponseBody
+    public String test1(){
+        return "test";
+    }
+}
---
+++ src/test/java/com/code/qrcodeback/QRcodebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.qrcodeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QRcodebackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
